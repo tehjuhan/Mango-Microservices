445ecc56b73edadf87261344f4b775fa269a0c78
Kelvin Teh <kelvin.teh@motorolasolutions.com>
2024-07-08 20:04:47 +1000

    Lesson#28 Completed
    Register Services in Program Class File

---
+++ Mango.Web/Service/CouponService.cs
@@ -0,0 +1,46 @@
+﻿
+using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService) 
+        { 
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponAsync(string couponeCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
