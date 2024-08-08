22b5b5776799511c0fc6daa0fc74dc540f87a328
Kelvin Teh <kelvin.teh@motorolasolutions.com>
2024-08-08 22:01:42 +1000

    Lesson#57 Completed
    Register in Action with Role

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -36,6 +36,42 @@ namespace Mango.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDto obj)
+        {
+            ResponseDto result = await _authService.RegisterAsync(obj);
+            ResponseDto assignRole;
+
+            if (result != null && result.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(obj.Role))
+                {
+                    obj.Role = SD.RoleCustomer;
+                }
+
+                assignRole = await _authService.AssingRoleAsync(obj);
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    TempData["success"] = "Registration Successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            //else
+            //{
+            //    TempData["error"] = result.Message;
+            //}
+
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem { Text=SD.RoleAdmin, Value=SD.RoleAdmin },
+                new SelectListItem { Text=SD.RoleCustomer, Value=SD.RoleCustomer },
+            };
+
+            ViewBag.RoleList = roleList;
+
+            return View(obj);
+        }
+
         [HttpGet]
         public IActionResult Logout()
         {
